2b457813e0093c3f94578643647ca0c0933309cd
pauquetj <35800109+pauquetj@users.noreply.github.com>
2019-03-26 13:23:26 +0100

    Update read.c

--- read.c
+++ read.c
@@ -1,5 +1,5 @@
 
-int lecture ( int fd, Inode inode, int taille, char *text) {
+void lecture ( int fd, Inode inode, int taille, char *text) {
   bzero(text, sizeof(text));
   lseek( fd , inode, SEEK_SET);
   read(fd, text, taille);
