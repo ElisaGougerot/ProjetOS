5df4eba504ac2d69db57bfb7d548465329903c26
ElisaGougerot <35921710+ElisaGougerot@users.noreply.github.com>
2019-03-26 13:20:20 +0100

    Add files via upload

--- ecrire.c
+++ ecrire.c
@@ -1,7 +1,5 @@
-int ecriture(int fd, Inode inode, int taille, char *text){
+void ecriture(int fd, Inode inode, int taille, char *text){
 
   lseek(fd, inode, SEEK_SET);
   text = write(inode,taille,DIM,fd);
-
-  return text;
 }
